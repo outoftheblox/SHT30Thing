3202cc9ca4c1b6e211baf4040105290e79a4deb6
Gerben <info@gerbenvaneerten.nl>
2023-11-26 18:26:51 +0100

    Update

--- src/SHT30Thing.ino
+++ src/SHT30Thing.ino
@@ -8,9 +8,9 @@ Thing thing;
 BlinkPattern led(BUILTIN_LED);
 SHT3X sht30(0x45);
 
-BlinkPattern::Pattern<2> panic{{1,1},25};
-BlinkPattern::Pattern<2> start{{1,9},25};
-BlinkPattern::Pattern<2> normal{{1,39},25};
+BlinkPattern::Pattern<2> panic{{1, 1}, 25};
+BlinkPattern::Pattern<2> start{{1, 9}, 25};
+BlinkPattern::Pattern<2> normal{{1, 39}, 25};
 
 void setup()
 {
@@ -18,15 +18,19 @@ void setup()
     Serial.println();
     Serial.println("Client:" + thing.clientId());
 
+    led.begin();
     led.setPattern(start);
 
-    thing.onStateChange([](const String& msg){
+    thing.onStateChange([](const String &msg)
+    {
         Serial.println(msg);
     });
 
     thing.begin();
 
-    thing.addSensor("sht30/humidity" + thing.clientId(), 5000, [](Value& value){
+    String topicHumidity = "things/" + thing.clientId() + "/sht30/humidity";
+    thing.addSensor(topicHumidity, 5000, [](Value &value)
+    {
         sht30.get();
         float humidity = sht30.humidity;
         Serial.println(String("Read humidity ") + humidity);
@@ -34,7 +38,9 @@ void setup()
         value = humidity;
     });
 
-    thing.addSensor("sht30/temperature" + thing.clientId(), 5000, [](Value& value){
+    String topicTemperature = "things/" + thing.clientId() + "/sht30/temperature";
+    thing.addSensor(topicTemperature, 5000, [](Value &value)
+    {
         sht30.get();
         float temperature = sht30.cTemp;
         Serial.println(String("Read temperature ") + temperature);
@@ -42,10 +48,11 @@ void setup()
         value = temperature;
     });
 
-    thing.addActuator(thing.clientId() + "/sht30/display", [](Value& value){
+    String topicDisplay = "things/" + thing.clientId() + "/display";
+    thing.addActuator(topicDisplay, [](Value &value)
+    {
         Serial.println("Got " + String(value));
     });
-
 }
 
 void loop()
