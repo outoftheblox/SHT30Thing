cb29da0a1a9a0b409bd309d5a2e86ad982b878fb
Gerben <info@gerbenvaneerten.nl>
2023-11-27 12:13:50 +0100

    JSON output

--- src/SHT30Thing.ino
+++ src/SHT30Thing.ino
@@ -1,6 +1,7 @@
 #include <Thing.h>
 #include <BlinkPattern.h>
 #include <WEMOS_SHT3X.h>
+#include <ArduinoJson.h>
 
 using namespace ootb;
 
@@ -28,8 +29,27 @@ void setup()
 
     thing.begin();
 
+
+    String topic = "things/" + thing.clientId() + "/sht30/values";
+    thing.addSensor(topic, 5000, [](Value &value)
+    {
+        sht30.get();
+        float humidity = sht30.humidity;
+        float temperature = sht30.cTemp;
+        const int capacity = JSON_OBJECT_SIZE(3);
+        StaticJsonBuffer<capacity> jb;
+        JsonObject& doc = jb.createObject();
+        //DynamicJsonDocument doc(1024);
+        doc["humidity"] = humidity;
+        doc["temperature"] = temperature;
+        String output;
+        doc.printTo(output);
+        value = output;
+        Serial.println(output);
+    });
+
     String topicHumidity = "things/" + thing.clientId() + "/sht30/humidity";
-    thing.addSensor(topicHumidity, 5000, [](Value &value)
+    thing.addSensor(topicHumidity, 60000, [](Value &value)
     {
         sht30.get();
         float humidity = sht30.humidity;
@@ -39,7 +59,7 @@ void setup()
     });
 
     String topicTemperature = "things/" + thing.clientId() + "/sht30/temperature";
-    thing.addSensor(topicTemperature, 5000, [](Value &value)
+    thing.addSensor(topicTemperature, 60000, [](Value &value)
     {
         sht30.get();
         float temperature = sht30.cTemp;
